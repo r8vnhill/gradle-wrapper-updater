28a96f4a3daf3f118001a1d0411941bd752633b2
R8V <islaterm@gmail.com>
2022-05-09 16:01:46 -0400

    Created repo

---
+++ src/main/kotlin/cl/ravenhill/grawup/GradleWrapperUpdater.kt
@@ -0,0 +1,12 @@
+package cl.ravenhill.grawup
+
+/*
+ * "gradle-wrapper-updater" (c) by R8V.
+ * "gradle-wrapper-updater" is licensed under a
+ * Creative Commons Attribution 4.0 International License.
+ * You should have received a copy of the license along with this
+ * work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
+ */
+fun main() {
+  println("Hello, world!")
+}
